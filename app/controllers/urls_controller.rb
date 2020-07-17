5aa61d47f60b393d012b81036a3c7111ac8299d1
archetype2142 <malhotraritwick2011@gmail.com>
2020-07-17 20:53:48 +0200

    Add

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -53,7 +53,7 @@ private
 
 	def sanitize url
 		url.strip!
-		sanitized_url = url.downcase.gsub(/(https?:\/\/)|(www\.)/, "")
+		sanitized_url = url.gsub(/(https?:\/\/)|(www\.)/, "")
 		sanitized_url.slice!(-1) if sanitized_url[-1] == "/"
 		sanitized_url = "http://#{sanitized_url}"
 	end
